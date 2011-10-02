c1cb54be0a76c44237af9a8a9caaa6e3db9edf05
Alfonso Boza <me@alfyboza.com>
2011-10-02 17:09:38 -0400

    More JSLint compliancy changes.

--- javascripts/cartography.js
+++ javascripts/cartography.js
@@ -160,15 +160,12 @@
                     results.results = result;
                     if (typeof location.onSuccess === "function") {
                         location.onSuccess(results);
-                    }
-                    else {
+                    } else {
                         $this.trigger($.cartography.events.Geocode.SUCCESS, results);
                     }
-                }
-                else if (typeof location.onFailure === "function") {
+                } else if (typeof location.onFailure === "function") {
                     location.onFailure(results);
-                }
-                else {
+                } else {
                     $this.trigger($.cartography.events.Geocode.FAILURE, results);
                 }
             });
@@ -209,8 +206,7 @@
                 // Create a map marker if given a coordinate.
                 if (location.id !== undefined && location.id !== "anonymous") {
                     markers[location.id] = marker;
-                }
-                else {
+                } else {
                     markers.anonymous.push(marker);
                 }
             } else if (location.address) {
@@ -316,30 +312,29 @@
         var method = options.method,
             provider = options.provider,
             instance;
-    
-        // Create instance of provider.
+        
+        // Determine what operation to perform.
         if (typeof method === "string") {
+            // Get instance to provider.
             instance = $(this).data(ns);
             
+            // Call provider's method.
             if (!instance || !instance.isCartography) {
                 $.error("jQuery Cartography has not been initialized.");
-            }
-            else if (typeof instance[method] === "function") {
+            } else if (typeof instance[method] === "function") {
                 return instance[method].call(this, options);
-            }
-            else {
+            } else {
                 $.error("Requested method not supported by jQuery Cartography.");
             }
-        }
-        else if (typeof Provider[provider] === "function") {
+        } else if (typeof Provider[provider] === "function") {
+            // Create instance of provider.
             if (!Provider[provider].prototype.isCartography) {
                 Provider[provider].prototype = Provider();
             }
             
             $(this).data(ns, new Provider[provider](options, this));
-        }
-        // Otherwise, notify of invalid provider.
-        else {
+        } else {
+            // Otherwise, notify of invalid provider.
             $.error("Unrecognized jQuery Cartography provider.");
         }
     }
@@ -379,12 +374,12 @@
             context = $(context);
         }
         
-        // If jQuery, call the Cartography's geocode() method.
+        // Ensure context is jQuery.
         if (context.selector !== undefined) {
+            // And then geocode.
             context.cartography("geocode", location);
-        }
-        // Otherwise, notify of error.
-        else {
+        } else {
+            // Otherwise, notify of error.
             $.error("Cannot determine context to geocode in.");
         }
     };
